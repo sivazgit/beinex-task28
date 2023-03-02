7ca5cb617dc7c4ee3eea25a521289d23b42a1b14
unknown <sivapriya3256@gmail.com>
2023-03-02 20:23:51 +0530

    feb28

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthenticationGuardService } from './authentication-guard.service'
-import { DeactivationGuardService } from './deactivation-guard.service';
 
 const routes: Routes = [
   {
     path:'',component:LoginComponent
   },
   {
-    path:'dashboard',component:DashboardComponent , canActivate: [AuthenticationGuardService], canDeactivate:[DeactivationGuardService]
+    path:'dashboard',component:DashboardComponent , canActivate: [AuthenticationGuardService]
   }
 ];
 
